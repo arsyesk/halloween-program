b6dbbc17eefadad42e3bd74946d8f8e733c28ee0
arsalan eskandrian <arsalan.eskandrian@elev.ga.ntig.se>
2024-10-17 10:26:36 +0200

    frank added

--- halloween.py
+++ halloween.py
@@ -1,7 +1,7 @@
 # halloween.py
 
 # Fråga användaren om deras favorit-Halloween-karaktär
-karaktar = input("Vilken är din favorit-Halloween-karaktär? (vampyr, zombie, spöke): ").lower()
+karaktar = input("Vilken är din favorit-Halloween-karaktär? (vampyr, zombie, spöke, frank): ").lower()
 
 # Använd en if-sats för att ge ett svar baserat på användarens val
 if karaktar == "vampyr":
@@ -10,5 +10,7 @@ elif karaktar == "zombie":
     print("Hjärnor... hjärnor...")
 elif karaktar == "spöke":
     print("Buuuu!")
+elif karaktar == "frank":
+    print("Baaaah!")
 else:
     print("Det låter som en spännande karaktär!")
