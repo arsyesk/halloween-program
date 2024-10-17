86ee35976314fb5d94b47b5c9243d113b59eebdc
arsalan eskandrian <arsalan.eskandrian@elev.ga.ntig.se>
2024-10-17 10:23:14 +0200

    Initial commit: Skapade projekt och README-fil

---
+++ halloween.py
@@ -0,0 +1,14 @@
+# halloween.py
+
+# Fråga användaren om deras favorit-Halloween-karaktär
+karaktar = input("Vilken är din favorit-Halloween-karaktär? (vampyr, zombie, spöke): ").lower()
+
+# Använd en if-sats för att ge ett svar baserat på användarens val
+if karaktar == "vampyr":
+    print("Blod är livet!")
+elif karaktar == "zombie":
+    print("Hjärnor... hjärnor...")
+elif karaktar == "spöke":
+    print("Buuuu!")
+else:
+    print("Det låter som en spännande karaktär!")
